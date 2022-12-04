fe8164118efd2b9ed5158f1a4d9b379e5552f3ef
Hanna Mandzirokha <hanna.mandzirokha@gmail.com>
2022-12-04 19:50:13 +0100

    refactoring

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,8 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
+import { ContactForm } from '../ContactForm/ContactForm';
+import { ContactList } from '../ContactList/ContactList';
+import { Filter } from '../Filter/Filter';
 import { Box, Container, Title } from './App.styled';
 
 export class App extends Component {
